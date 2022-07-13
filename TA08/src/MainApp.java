60859c53307afcc0d77fdcf9e4988ac1d1380fa6
Celestinvib <celestinvib@gmail.com>
2022-07-13 10:11:53 +0200

    Persona v1.2

--- TA08/src/MainApp.java
+++ TA08/src/MainApp.java
@@ -3,12 +3,14 @@ public class MainApp {
 
 	public static void main(String[] args) {
 	 
-		Persona persona1 = new Persona();
-		Persona persona2 = new Persona("Marcelo",34,"H");
+		Persona persona1 = new Persona("47785643A");
+		Persona persona2 = new Persona("Marcela",34,"47785643C","F");
 		Persona persona3 = new Persona("Leo",31,"37785643A","H",60.5,1.75);
 		
 		
-		
+		System.out.println("Persona 1:\n"+persona1.getNombre()+" - "+persona1.getEdad()+" Años- "+persona1.getDni()+" - "+persona1.getSexo()+" - "+persona1.getPeso()+"kg - "+persona1.getAltura()+"m");
+		System.out.println("Persona 2:\n"+persona2.getNombre()+" - "+persona2.getEdad()+" Años- "+persona2.getDni()+" - "+persona2.getSexo()+" - "+persona2.getPeso()+"kg - "+persona2.getAltura()+"m");
+		System.out.println("Persona 3:\n"+persona3.getNombre()+" - "+persona3.getEdad()+" Años- "+persona3.getDni()+" - "+persona3.getSexo()+" - "+persona3.getPeso()+"kg - "+persona3.getAltura()+"m");
 		
 	}
 
--- TA08/src/Persona.java
+++ TA08/src/Persona.java
@@ -1,31 +1,29 @@
 
 public class Persona {
 	
-	private String nombre;
+	private final String sexoDefecto = "H";	
 	
-	private int edad;
+	private String nombre = "";
+	
+	private int edad = 0;
 
 	private String dni;
 
-	private String sexo;
+	private String sexo = sexoDefecto;
 	
-	private double peso;
+	private double peso = 0;
 
-	private double altura;
+	private double altura = 0;
 
-	private final String sexoDefecto = "H";	
 	
-	public Persona() {
-		this.nombre = "";
-		this.edad = 0;
-		this.sexo = sexoDefecto;
-		this.peso = 0;
-		this.altura = 0;		
+	public Persona(String dni) {
+		this.dni = dni;		
 	}
 	
-	public Persona(String nombre, int edad, String sexo) {
+	public Persona(String nombre, int edad,String dni, String sexo) {
 		this.nombre = nombre;
 		this.edad = edad;
+		this.dni = dni;
 		this.sexo = sexo;
 	}
 
@@ -37,10 +35,56 @@ public class Persona {
 		this.peso = peso;
 		this.altura = altura;
 	}
-	
-	
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
 	
 	
 	
+		
 }
