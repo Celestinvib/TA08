d8d749e08f5d5f3f992d7fbc2fa004dd0525b696
Celestinvib <celestinvib@gmail.com>
2022-07-13 09:41:30 +0200

    Clase Persona acabada

---
+++ TA08/src/MainApp.java
@@ -0,0 +1,15 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+	 
+		Persona persona1 = new Persona();
+		Persona persona2 = new Persona("Marcelo",34,"H");
+		Persona persona3 = new Persona("Leo",31,"37785643A","H",60.5,1.75);
+		
+		
+		
+		
+	}
+
+}
